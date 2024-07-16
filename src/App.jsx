739ddb45851c88f7e82adcb6582cf4d7f4dc65ab
szlima <suzanelima@outlook.com>
2024-07-16 10:04:36 -0300

    Início do projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Night sky</h1>
+        <nav className="menu">menu</nav>
+      </header>
+      <main className="night-sky">night-sky</main>
+      <div className="toolbox">toolbox</div>
+    </>
+  );
+}
+
+export default App;
